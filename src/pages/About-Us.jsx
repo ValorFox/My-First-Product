e16d98a600f098a85b7eadf43d481efa54d6cd94
ValorFox <nikhilkashyap7508@gmail.com>
2024-03-30 03:12:20 +0530

    second commit

--- src/pages/About-Us.jsx
+++ src/pages/About-Us.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function About() {
   return (
     <div className="w-full py-8 text-gray-200">
-      Hello My name is Nikhil Kashyap I am founder of this page
+      Hello My name is Nikhil Kashyap and I am founder of this page
     </div>
   );
 }
